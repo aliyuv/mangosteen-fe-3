f89850cf9c0c99cbd1362fed2893db24192506d5
aliyu <1324149139@qq.com>
2022-10-20 17:01:59 +0800

    使用 SVG Sprites

--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -3,7 +3,9 @@ import cloud from '../../assets/icons/cloud.svg';
 import {FunctionalComponent} from "vue";
 export const Forth: FunctionalComponent = () => (
     <div class={s.card}>
-      <img src={cloud} alt=""/>
+      <svg>
+          <use xlinkHref="cloud"></use>
+      </svg>
       <h2>每日提醒<br/>不遗漏每一笔账单</h2>
     </div>
 )
