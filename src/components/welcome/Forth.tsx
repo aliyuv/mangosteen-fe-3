13d9305e66e641c30d029624fb3354ac66cdd700
aliyu <1324149139@qq.com>
2022-10-21 14:26:26 +0800

    添加useSwipe

--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -4,7 +4,7 @@ import {FunctionalComponent} from "vue";
 export const Forth: FunctionalComponent = () => (
     <div class={s.card}>
       <svg>
-          <use xlinkHref="cloud"></use>
+          <use xlinkHref="#cloud"></use>
       </svg>
       <h2>每日提醒<br/>不遗漏每一笔账单</h2>
     </div>
